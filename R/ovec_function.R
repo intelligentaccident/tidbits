aedc933aa30d4ad7130feaeb5885b9e5cf3fe5dd
intelligentaccident <intelligentaccident@gmail.com>
2016-09-26 15:55:25 +0200

    New documentation

--- R/ovec_function.R
+++ R/ovec_function.R
@@ -1,10 +1,14 @@
-#' Function for generating a character vector from crossing vectors, separated by sep
+#' Crossed character vectors
 #'
-#' @param ... A set of parameters that can be interpreted as character vectors.
-#' @param FUN A function to combine parameters. Defaults to function(a, b) paste0(a, b)
+#' @param ... A set of parameters that can be coerced by as.character.
+#' @param FUN A function to combine parameters. Defaults to \code{function(a, b) paste0(a, b)}
+#' @return Character vector containing all combinations of provided parameters
+#' @description Function which uses \code{\link{outer}} and provided function FUN to generate a character vector of all possible combinations of
+#' the provided parameters, each coerced by as.character
 #' @keywords outer, vector, character
 #' @examples
-#' ovec(c("x", "y", "z"), 5:10)
+#' ovec(c("x", "y", "z"), 1:3)
+#' # c("x1", "x2", "x3", "y1", "y2", "y3", "z1", "z2", "z3")
 
 ovec <- function(..., FUN = function(a, b) paste0(a, b)) {
   args <- list(...)
