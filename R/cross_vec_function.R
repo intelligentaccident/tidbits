819adc511c4c400969988ae94dd7c4c41be50526
Kim Rand-Hendriksen <Kim Rand-Hendriksen>
2016-09-27 12:02:52 +0200

    cross_vec

--- R/cross_vec_function.R
+++ R/cross_vec_function.R
@@ -1,18 +1,18 @@
 #' Crossed character vectors
 #'
 #' @param ... A set of parameters that can be coerced by as.character.
-#' @param FUN A function to combine parameters. Defaults to \code{function(a, b) paste0(a, b)}
+#' @param FUN A function to combine parameters. Defaults to \code{function(...) paste(..., collapse = "")}
 #' @return Character vector containing all combinations of provided parameters
 #' @description Returns a char vector containing all combinations of provided vectors, each coerced by as.character.
 #' @keywords outer, vector, character
 #' @examples
 #' ovec(c("x", "y", "z"), 1:3)
 #' # c("x1", "x2", "x3", "y1", "y2", "y3", "z1", "z2", "z3")
+#' 
 
-ovec <- function(..., FUN = function(a, b) paste0(a, b)) {
+cross_vec <- function(..., FUN = function(...) paste(..., collapse = "")) {
   args <- list(...)
   larg <- length(args)
   for(narg in 1:larg) args[[narg]] <- as.character(args[[narg]])
-  if(larg < 2) return(unlist(args))
-  else return(as.vector(outer(args[[1L]], do.call(ovec, c(args = args[-1L], FUN = FUN )), FUN = FUN)))
+  apply(do.call(expand.grid, list(...)), MARGIN = 1, FUN = FUN)
 }
