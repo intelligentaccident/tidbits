cdf61b8f41fbf45accf33730312c5aae70cbf296
Kim Rand-Hendriksen <Kim Rand-Hendriksen>
2016-09-27 16:42:09 +0200

    documents

--- R/multimatch_function.R
+++ R/multimatch_function.R
@@ -3,8 +3,8 @@
 #' @param dfx data.frame with rows to be matched
 #' @param dftable data.frame with rows to be matched against
 #' @return \code{multimatch} returns a vector of position of (first) matches of its first argument in its second.
-#' @return \code{multin} corresponds to \code{\link{\%in\%}} 
-#' @return \code{multwhich} corresponds to \code{\link{which}}
+#' @return \code{multin} corresponds to \code{\link[base]{\%in\%}} 
+#' @return \code{multwhich} corresponds to \code{\link[base]{which}} 
 #' @usage 
 #' @aliases multin, multwhich
 #' @description Returns a match performed at the level of rows in data.frames.
@@ -14,6 +14,7 @@ multimatch <- function(dfx, dftable) {
   return((match(interaction(as.list(dfx)), interaction(as.list(dftable)))))
 }
 
+#' @describeIn multimatch corresponds to \code{\link[base]{\%in\%}}
 multin <- function(dfx, dftable) !is.na(multimatch(dfx, dftable))
-
+#' @describeIn multimatch corresponds to \code{\link[base]{which}} 
 multwhich <- function(dfx, dftable) which(multin(dfx, dftable))
